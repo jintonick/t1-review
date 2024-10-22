f145a53b872aea73652f6c158dc7f41f8676f880
jintonick <viktor.krasuyk@gmail.com>
2024-10-22 11:55:20 +0300

    first commit

--- src/pages/projects-page/blocks/projects-page-filter.tsx
+++ src/pages/projects-page/blocks/projects-page-filter.tsx
@@ -1,48 +1,40 @@
-import React, {useState} from "react";
-import {Button, DatePicker, Input, InputNumber, Slider} from "antd";
+import React from "react";
+import {Button, DatePicker, Input} from "antd";
 import CustomSelect from "@app/components/select/select";
 import search from "../../../imgs/search.icon.svg";
 import dayjs from "dayjs";
 
 const options = [
-  { value: "option1", label: "Option 1" },
-  { value: "option2", label: "Option 2" },
-  { value: "option3", label: "Option 3" },
+  { value: "option1", label: "Junior" },
+  { value: "option2", label: "Middle" },
+  { value: "option3", label: "Senior" },
 ];
 
 const ProjectsPageFilter = () => {
-  const [stepsCount, setStepsCount] = useState<number>(5);
   const dateFormat = "MM.DD.YY";
 
   return (
     <div className="bg-white flex-grow w-full  max-w-[291px] py-[24px] pr-[27px] pl-[34px] flex flex-col">
-      <h1 className="font-bold text-[24px]">Filters</h1>
-      <div className="flex justify-between mt-[20px]">
-        <DatePicker className="border-[#DCE0E5] w-[108px] h-[34px] rounded-[10px]" defaultValue={dayjs("2015/01/01", dateFormat)} format={dateFormat} />
-        <DatePicker className="border-[#DCE0E5] w-[108px] h-[34px] rounded-[10px]" defaultValue={dayjs("2015/01/01", dateFormat)} format={dateFormat} />
-      </div>
-      <div className='mt-[20px] pb-[25px] border-b-[1px] border-[#DCE0E5]'>
-        <div className="flex justify-between items-center font-medium text-[#5D697E]">
-          <h1 className="text-[14px]">Duration project (min):</h1>
-          <InputNumber className="border-[#DCE0E5] w-[75px] h-[34px] rounded-[10px]" min={1} max={10} defaultValue={3} />
-        </div>
-        <Slider className="fill-[#598BF2]" min={2} max={10} value={stepsCount} onChange={setStepsCount} />
-      </div>
+      <h1 className="font-bold text-[24px]">Фильтр</h1>
       <div className="pt-[18px] flex flex-col gap-[18px]">
         <div>
-          <label className="text-[15px] text-[#2E3842] font-medium ml-[4px]">Name</label>
-          <Input className="h-[34px] rounded-[10px] border-[#DCE0E5]" placeholder="Enter" />
+          <label className="text-[15px] text-[#2E3842] font-medium ml-[4px]">Найти ревьювера</label>
+          <Input className="h-[34px] rounded-[10px] border-[#DCE0E5]" placeholder="Найти" />
         </div>
         <div className='flex flex-col'>
-          <label className="text-[15px] text-[#2E3842] font-medium ml-[4px]">Status</label>
+          <label className="text-[15px] text-[#2E3842] font-medium ml-[4px]">Компетенция</label>
           <CustomSelect
             options={options}
-            placeholder="Choose an option"
+            placeholder="Выберите компетенцию"
             height="34px"
           />
         </div>
       </div>
-      <Button className="mt-[20px] h-[34px] border-none bg-[#598BF2] rounded-[10px] text-white font-bold text-[15px]"><img src={search}/><h1>Search</h1></Button>
+      <div className="flex justify-between mt-[20px]">
+        <DatePicker className="border-[#DCE0E5] w-[108px] h-[34px] rounded-[10px]" defaultValue={dayjs("2015/01/01", dateFormat)} format={dateFormat} />
+        <DatePicker className="border-[#DCE0E5] w-[108px] h-[34px] rounded-[10px]" defaultValue={dayjs("2015/01/01", dateFormat)} format={dateFormat} />
+      </div>
+      <Button className="mt-[20px] h-[34px] border-none bg-[#598BF2] rounded-[10px] text-white font-bold text-[15px]"><img src={search}/><h1>Поиск</h1></Button>
     </div>
   );
 };
